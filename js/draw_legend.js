9a9577d6b56310376042ef94e6fb92d0e94e7463
bekaa <sci.kd.eg@gmail.com>
2017-03-01 17:40:04 +0200

    added buttons, decoration/documentation to the code

--- js/draw_legend.js
+++ js/draw_legend.js
@@ -6,36 +6,36 @@ function draw_legend( legend_svg_g, width, height, data)
 		gap = 22,
 		cx = 150,
 		cy = 200,
-		nan_percent = 0.0484,
 		total_loans_sum = 113937.0,
 		nan_count = 5515.0;
-
-		// add arc for Total loans.
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	//	first draw the two circles/arc
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	//  1-  for Total loans.
 	var total_arc = d3.svg.arc()
 			.startAngle(0)
 			.endAngle(twoPi * (1 - (nan_count / total_loans_sum) ) )
 			.innerRadius(0 + gap * radius)
 			.outerRadius(20 + gap * radius);
-
 	legend_svg_g.append("path")
 			.attr("transform", "translate(" + cx + "," + cy + ") rotate(180)")
 			.attr("d", total_arc)
 			.attr('class','total_arc');
-
-	// add arc for states.
+	//******************************
+	// 2 -  for states.
 	var state_arc = d3.svg.arc()
-	.startAngle(0)
-	.endAngle(twoPi * .9)
-	.innerRadius((0 + gap * radius) * .7 )
-	.outerRadius((20 + gap * radius) * .65);
-
+		.startAngle(0)
+		.endAngle(twoPi * .9)
+		.innerRadius((0 + gap * radius) * .7 )
+		.outerRadius((20 + gap * radius) * .65);
 	legend_svg_g.append("path")
 			.attr("transform", "translate(" + 400 + "," + cy + ") rotate(180)")
 			.attr("d", state_arc)
 			.attr('class','state_arc');
-
-	// add text inside the circle
-	// the bigger circle :
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	//	second  add text inside the circles
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	// 1 -  for total loans :
 	var text = legend_svg_g.append("text")
 		.attr("x", cx - 40 )
 		.attr("y", cy - 50)
@@ -44,7 +44,7 @@ function draw_legend( legend_svg_g, width, height, data)
 	text.append('tspan')
 		.text('Total');
 	text.append('tspan')
-		.text( (1-nan_percent)*100 + '%' )
+		.text( (1 - (nan_count / total_loans_sum))*100 + '%' )
 		.attr('id','percent')
 		.attr('x',cx - 50)
 		.attr('dy',50);
@@ -53,58 +53,65 @@ function draw_legend( legend_svg_g, width, height, data)
 		.attr('id','loans')
 		.attr('x',cx - 60 )
 		.attr("dy",40);
-		//*************************
-		// the smaller one :
-		var text = legend_svg_g.append('text')
-			.attr("x", cx + 200 )
-			.attr("y", cy - 20)
-			.attr('class','state_text');
+	//*************************
+	// 2 - for states :
+	var text = legend_svg_g.append('text')
+		.attr("x", cx + 200 )
+		.attr("y", cy - 20)
+		.attr('class','state_text');
 
-		text.append('tspan')
-				.attr('id','state');
-		text.append('tspan')
-				.attr('id','percent')
-				.attr('x', cx + 215)
-				.attr('dy',40);
-		text.append('tspan')
-				.attr('id','loans')
-				.attr('x',cx + 195 )
-				.attr("dy",30);
-		// UPDATE it when mouse hover over a state.
-		d3.selectAll('path.states')
-			.on('mouseover', function(){
-				var d     = this.__data__,
-					state   = d.properties.name,
-					loans   = +d.properties.loans;
-				text.select('#state').text(state);
-				text.select('#loans').text( loans.toLocaleString() + ' Loans');
-				text.select('#percent').text((loans/total_loans_sum * 100).toFixed(2) + '%' );
-				state_arc.endAngle( twoPi * loans/data[0].loans );
-				legend_svg_g.select('path.state_arc')
-					.attr('d',state_arc);
-			})
-	//****************************************
-	// add a  list, (some notes, under the arc)
+	text.append('tspan')
+			.attr('id','state');
+	text.append('tspan')
+			.attr('id','percent')
+			.attr('x', cx + 215)
+			.attr('dy',40);
+	text.append('tspan')
+			.attr('id','loans')
+			.attr('x',cx + 195 )
+			.attr("dy",30);
+	// UPDATE it when mouse hover over a state.
+	d3.selectAll('path.states')
+		.on('mouseover', function(){
+			var d     = this.__data__,
+				state   = d.properties.name,
+				loans   = +d.properties.loans;
+			text.select('#state').text(state);
+			text.select('#loans').text( loans.toLocaleString() + ' Loans');
+			text.select('#percent').text((loans/total_loans_sum * 100).toFixed(2) + '%' );
+			state_arc.endAngle( twoPi * loans/data[0].loans );
+			legend_svg_g.select('path.state_arc')
+				.attr('d',state_arc);
+		})
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	//	third  add some notes below the circles
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 	var notes = legend_svg_g.append('g')
 					.attr('class','notes')
 				.append('text')
-					.attr('dx', 0)
+					.attr('dx', -5)
 					.attr('dy', 360);
 
 	notes.append('tspan')
-			.text("* There is a 97,529 different borrowers with 113,937 loans.");
+			.text('* The rest 4.84%(5,515 Loans) doesn\'t include a state (NaN values). ')
+			.text();
 	notes.append('tspan')
-			.attr('x',0)
+			.attr('x',-5)
 			.attr('dy',30)
-			.text('* As many of them made loans from different states,');
+			.text("* There are 113,937 loans from 97,529 different borrowers.")
 	notes.append('tspan')
-			.attr('x',20)
+			.attr('x',-5)
+			.attr('dy',30)
+			.text('* Some of the borrowers made loans from more than one state,');
+	notes.append('tspan')
+			.attr('x',15)
 			.attr('dy',20)
-			.text(' we will be dealing with the number of loans not number of borrowers.');
+			.text(' so we can\'t assign a state for each borrower.');
 	notes.append('tspan')
-			.attr('x',0)
+			.attr('x',-5)
 			.attr('dy',30)
-			.text('* There is a 5515(4.84%) loans which doesn\'t include a state (NaN values).');
-
+			.text('* the length of the smaller arc is proportional to the max number of loans in a state (CA). ');
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	// return the two arc objects, will be used for animation.
 	return [ total_arc, state_arc ];
 };
--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ d3.queue()
 	.defer(d3.csv,'data/borrower_states.csv')
 	.await(main);
 
-
+//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 function main(error, states_geo, states_borrower)
 {
 	var margin = 20,
@@ -13,9 +13,12 @@ function main(error, states_geo, states_borrower)
 		width = 1500,
 		height = 600,
 		twoPi = 2.0 * Math.PI,
-		total_loans_sum = 113937.0;
+		total_loans_sum = 113937.0,
+		nan_count = 5515.0;
 
+	// add title.
 	d3.select('body').append('h3').attr('class','title').text('Data visualization of the borrowers at Prosper.com');
+	// add some text below the title.
 	d3.select('body').append('p').attr('class','info')
 			.html('Prosper is America’s first marketplace lending platform,\
 			 with over $8 billion in funded loans. for more information visit <a href="https://www.prosper.com/">Prosper.com</a>. <br><br> \
@@ -30,8 +33,11 @@ function main(error, states_geo, states_borrower)
 	var legend = svg.append('g').attr('class','legend')
 								.attr('transform','translate('+mapWidth+',0)');
 
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 	//  append count from states_borrower to states_geo
-	// also change the variable "id" [index] for sorting, the highest count value will take [1] and the lowest would take the highest index.
+	// also change the variable "id" [index] for sorting,
+	//	  the highest count value will take [1] and the lowest would take the highest index.
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 	var index = 0;
 	for (var i = 0; i < states_borrower.length; i++)
 	{
@@ -43,31 +49,42 @@ function main(error, states_geo, states_borrower)
 			if (geo_state == state)
 			{
 				states_geo.features[j].properties.loans = loans;
-				states_geo.features[j].id = index
-				index++ ;
+				states_geo.features[j].id = index;
+				index++;
 				break;
 			}
 		}
 	}
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	// call the two function from the two files  draw_map , and draw_legend
 	var color_scale = draw_map(map, mapWidth, height, states_geo);
 	var arc_objects  = draw_legend(legend, legendWdith, height, states_borrower);
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 
 	/***************************************
 	************ ANIMATION ******************
 	*****************************************/
-	var total = 0.0;
-	var loans_extent = d3.extent(states_borrower, function(d){ return +d.loans; })
-	var time_ratio = d3.scale.linear().domain(loans_extent).range([0,2]);
+	var total = 0.0, // used by animate() to sum up loans.
+		stop_exec_command = false, // a flag used to tell animate() to stop execution.
+		running_flag = false; // a flag used to figure wether animate() is running or not.
+	var loans_extent = d3.extent(states_borrower, function(d){ return +d.loans; }) //get maximum and minimum number of loans.
+	var time_ratio = d3.scale.linear().domain(loans_extent).range([0,2]); // convert loans to range [0-2](used by setTimeout()).
 	function animate(id)
 	{
-		//debugger;
+		// the following  will be executed if the user pressed the skip button.
+		if (stop_exec_command == true )
+		{
+			running_flag = false;
+			stop_exec_command = false;
+			return;
+		}
 		//******************************
 		//        UPDATE MAP
 		//******************************
 		map.select('path#path'+(id-1))
 			.attr('class','states');
 
-		map.select('path#path'+id+'.states')
+		map.select('path#path'+id)
 			.attr('class','add_map_dec')
 			.style('fill',function(d)
 				{
@@ -79,18 +96,17 @@ function main(error, states_geo, states_borrower)
 		//***********************************
 		//calculate ratio
 		var state = states_borrower[id].state,
-				loans = +states_borrower[id].loans;
+			loans = +states_borrower[id].loans;
 		total = total + loans;
 		var ratio = total / total_loans_sum;
 
 		// update the circle
-		//debugger;
 		arc_objects[0].endAngle(twoPi *  ratio  );
 		legend.select('path.total_arc')
 			.attr('d',arc_objects[0]);
 		arc_objects[1].endAngle( twoPi * loans/loans_extent[1] );
 		legend.select('path.state_arc')
-			.attr('d',arc_objects[1])
+			.attr('d',arc_objects[1]);
 
 		// update the text
 		var text = legend.select('.total_text');
@@ -103,24 +119,123 @@ function main(error, states_geo, states_borrower)
 		//*******************************************
 		//********************************
 		// call the function for all the states.
-		if (id == 49 ){
-			map.select('path#path'+(id))
-				.attr('class','states');
-			return;
+		if (id == 49 ) // indicate the last element(state) .
+		{
+			running_flag = false;
+			map.select('path#path'+(id)).attr('class','states');
 		}
 		//recursion
-		else {
-		 setTimeout(function(){animate(id+1)},200+1000*time_ratio(loans));
-		 debugger;
-		}
-
+		else  setTimeout(function(){animate(id+1)},200+1000*time_ratio(loans));
 	}
-	// clear all colors in the map first.
-	map.selectAll('path.states').style('fill','white');
-	// start animation
-	animate(0);
-}
 
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	// MAKE BUTTONS [ PLAY & SKIP ]
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	  var buttonsWidth = 650,
+	  	  buttonsHeight = 50;
+	  var buttons = svg.append('g')
+	  					.attr('class','buttons')
+						.attr('transform', 'translate(' + buttonsWidth + ',' + buttonsHeight +  ')');
+	//*****************************
+	// first button [ skip button ]
+	var skipButton = buttons.append('g').attr('class', 'skipButton');
+	skipButton.append("circle")
+		.attr("r", 30)
+	 	.attr("transform", "translate(" + 0 + "," + 0 + ")");
+ 	skipButton.append("path")
+	 .attr("d", "M-22,-30l60,30l-60,30z")
+	 .attr("transform", "translate(" +  (-7) + "," + 0 + ") scale(.4)");
+	skipButton.append("path")
+		.attr("d", "M-22,-30l60,30l-60,30z")
+		.attr("transform", "translate(" + 8 + "," + 0 + ") scale(.4)");
+	skipButton.append('rect')
+		.attr("transform", "translate(" + (-30) + "," + (-30) + ")")
+		.attr('width', 60)
+		.attr('height', 60)
+		.on("click", function() {
+          reanimate(true)
+        });
+	// update the tooltip.
+	$('.skipButton').tipsy({
+		gravity: 'w',
+		html: true,
+		title: function(){return 'skip'}
+	});
+	//**********************************
+	// second button [ play button ]
+	var gap = 100;
+	var playButton = buttons.append('g').attr('class', 'playButton')
+		.attr("transform", 'translate(' + gap + ' , 0 )' );
+	playButton.append("circle")
+		.attr("r", 30)
+		.attr("transform", 'translate(' + 0 + ' , 0 )' );
+	playButton.append("path")
+	 	.attr("d", "M-22,-30l60,30l-60,30z")
+	 	.attr("transform", 'translate(' +  0 + ', 0 ) scale(.4)');
+	playButton.append('rect')
+ 		.attr("transform", "translate(" + ( -30) + "," + (-30) + ")")
+ 		.attr('width', 60)
+ 		.attr('height', 60)
+ 		.on("click", function() {
+			reanimate()
+			//reanimate(true)
+		   //reanimate()
+         });
+	// update the tooltip.
+	 $('.playButton').tipsy({
+ 		gravity: 'w',
+ 		html: true,
+ 		title: function(){return 'play'}
+	});
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	//	function to call animate() for the first time,
+	//		and also when the play/skip buttons are clicked.
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	function reanimate(skip = false)
+	{
+		if(skip == true) // ie. user pressed skip button.
+		{
+			// set stop_exec_command to true so the function animate() stop it's execution.
+			stop_exec_command = true;
+			// restore colours and class name for the map.
+			map.selectAll('path.states, path.add_map_dec')
+			.attr('class', 'states')
+			.style('fill',function(d)
+			{
+				var loans = d.properties.loans;
+				return color_scale( loans );
+			});
+			// update the legend circle and it's text.
+			arc_objects[0].endAngle(twoPi * ( 1 - ( nan_count / total_loans_sum ) )  );
+			legend.select('path.total_arc')
+				.attr('d',arc_objects[0]);
+			var text = legend.select('.total_text');
+			text.select('#percent').text(((1 - nan_count / total_loans_sum)*100).toFixed(2) + '%' );
+			text.select('#loans').text( (total_loans_sum - nan_count).toLocaleString()+' Loans' );
+		}
+		else // first time animation or user pressed play button
+		{
+			// is the animation is running, send stop_exec_command signal to stop it.
+			if (running_flag == true) stop_exec_command = true;
+			var interval = setInterval(function(){
+				if ( running_flag == false ) // wait until the animate() function stop working.
+				{
+					// clear all colors in the map first.
+					map.selectAll('path.states, path.add_map_dec').attr('class', 'states').style('fill','white');
+					total = 0; // clear the sum of loans.
+					// start animation
+					running_flag = true;
+					animate(0);
+					clearInterval(interval); // stop setInterval command
+				}
+			}, 500);
+
+		}
+	}
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+	// call for the first time .
+	reanimate();
+}
 
 
 
@@ -130,10 +245,12 @@ function main(error, states_geo, states_borrower)
 **********************************************************
 These projects helped me alot,
 
-http://bl.ocks.org/ilyabo/1373263
+http://bl.ocks.org/ilyabo/1373263   >> tipsy example
+https://github.com/jaz303/tipsy     >> tipsy library
+https://bl.ocks.org/mbostock/6408735  >> buttons example
+http://codepen.io/anon/pen/NqWQNg   >> arc example
 http://bl.ocks.org/michellechandra/0b2ce4923dc9b5809922
 
-http://codepen.io/anon/pen/NqWQNg
 *********************************************************
 
 */
